557cdccb2e97ff3e9c5cb26587448d732638e560
xinkent <xinkent1994@gmail.com>
2023-07-01 22:20:42 +0900

    ヘルプページを改善

--- lib/screens/help.dart
+++ lib/screens/help.dart
@@ -11,55 +11,59 @@ class HelpPage extends StatelessWidget {
         appBar: AppBar(title: const Text('使い方')),
         body: Padding(
           padding: const EdgeInsets.only(top: 10, left: 20, right: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: const [
-              Text(
-                'このアプリの使い方',
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                    decoration: TextDecoration.underline),
-              ),
-              Text(
-                'このアプリではバドミントンやテニスのダブルスの試合の組み分けをすることができます。 \n'
-                'メンバー設定画面でメンバーを追加した後、「練習を開始する」ボタンを押してコート数と練習参加メンバーを設定するだけですぐに開始することができます',
-                style: TextStyle(
-                  fontSize: 18,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  'このアプリの使い方',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                      decoration: TextDecoration.underline),
                 ),
-              ),
-              SizedBox(height: 20),
-              Text(
-                '組み分けの選択方法',
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
+                Text(
+                  'このアプリではバドミントンやテニスのダブルスの試合の組み分けをすることができます。 \n'
+                  'メンバー設定画面でメンバーを追加した後、「練習を開始する」ボタンを押してコート数と練習参加メンバーを設定するだけですぐに開始することができます',
+                  style: TextStyle(
                     fontSize: 18,
-                    decoration: TextDecoration.underline),
-              ),
-              Text(
-                '試合組の決め方は以下のルールに従います \n'
-                '① 試合参加回数が少ないメンバーが優先的に試合に参加するようにします。 \n'
-                '② 同じ試合組が設定されることは可能な限り回避します。 \n\n'
-                '表示された試合組を変更したい場合は再度自動生成することも、手動でメンバーを変更することも可能です',
-                style: TextStyle(
-                  fontSize: 18,
+                  ),
                 ),
-              ),
-              SizedBox(height: 20),
-              Text(
-                '注意点',
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
+                SizedBox(height: 20),
+                Text(
+                  '組み分けの選択方法',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                      decoration: TextDecoration.underline),
+                ),
+                Text(
+                  '試合組の決め方は以下のルールに従います \n'
+                  '① 試合参加回数が少ないメンバーが優先的に試合に参加するようにします。 \n'
+                  '② 連続で休憩になるメンバーがなるべく発生しないようにします。 \n'
+                  '③ 男女が組み合わさる場合はミックスの組み合わせになるようにします \n'
+                  '④ 同じ試合組が設定されることは可能な限り回避します。 \n\n'
+                  '表示された試合組を変更したい場合は再度自動生成することも、手動でメンバーを変更することも可能です',
+                  style: TextStyle(
                     fontSize: 18,
-                    decoration: TextDecoration.underline),
-              ),
-              Text(
-                '練習を終了するとそれまでの試合回数がリセットされてしまうので注意してください \n',
-                style: TextStyle(
-                  fontSize: 18,
+                  ),
                 ),
-              ),
-            ],
+                SizedBox(height: 20),
+                Text(
+                  '注意点',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                      decoration: TextDecoration.underline),
+                ),
+                Text(
+                  '練習を終了するとそれまでの試合回数がリセットされてしまうので注意してください \n',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+              ],
+            ),
           ),
         ));
   }
